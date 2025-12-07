079ce23a5804f7a4675f3e26bd3b51bbf5482a44
Juan Diego <juandsilva028@gmail.com>
2025-12-06 20:17:01 -0500

    first commit

---
+++ src/main/java/com/managelab/managelab/run/managelab.java
@@ -0,0 +1,18 @@
+package com.managelab.managelab.run;
+
+import org.openxava.util.*;
+
+/**
+ * Ejecuta esta clase para arrancar la aplicación.
+ *
+ * Con OpenXava Studio/Eclipse: Botón derecho del ratón > Run As > Java Application
+ */
+
+public class managelab {
+
+	public static void main(String[] args) throws Exception {
+		DBServer.start("managelab-db"); // Para usar tu propia base de datos comenta esta línea y configura src/main/webapp/META-INF/context.xml
+		AppServer.run("managelab"); // Usa AppServer.run("") para funcionar en el contexto raíz
+	}
+
+}
